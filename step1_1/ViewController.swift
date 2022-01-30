8dd07539ca7020e982815f7220c7a3b85a75228f
ChanceMKR <87632730+ChanceMKR@users.noreply.github.com>
2022-01-30 10:09:36 +0900

    step1

--- step1_1/ViewController.swift
+++ step1_1/ViewController.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var userImageView: UIImageView!
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var depthLabel: UILabel!
+    @IBOutlet weak var widthLabel: UILabel!
+    @IBOutlet weak var heightLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        imageView.image = UIImage.init(named: "jattefjall")
+        
+        titleLabel.text = "HATTEFJÄLL"
+        
+        userImageView.image = UIImage.init(named: "macbook")
+        userNameLabel.text = "chance"
+        
+        priceLabel.text = "10,000"
+        depthLabel.text = "68cm"
+        widthLabel.text = "68cm"
+        heightLabel.text = "110cm"
+    }
 
 }
 
