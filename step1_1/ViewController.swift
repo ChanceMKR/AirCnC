fa7d1f505fd93f34289b656642d50270bee9324c
ChanceMKR <87632730+ChanceMKR@users.noreply.github.com>
2022-02-03 10:10:54 +0900

    step2

--- step1_1/ViewController.swift
+++ step1_1/ViewController.swift
@@ -8,7 +8,6 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var userImageView: UIImageView!
@@ -17,10 +16,58 @@ class ViewController: UIViewController {
     @IBOutlet weak var depthLabel: UILabel!
     @IBOutlet weak var widthLabel: UILabel!
     @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var rightButton: UIButton!
+    @IBOutlet weak var leftButton: UIButton!
+    @IBOutlet weak var likeButton: UIButton!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    var currentImageIndex = 0
+    let images = [
+        "hattefjall_1",
+        "hattefjall_2",
+        "hattefjall_3",
+        "hattefjall_4",
+        "hattefjall_5"
+    ]
+
+    
+    @IBAction func showNextImage(_ sender: Any){
+        currentImageIndex += 1
+        print(images[currentImageIndex])
+        showImage(index: currentImageIndex)
+    }
+    
+    @IBAction func showPrevImage(_ sender: Any){
+        currentImageIndex -= 1
+        print(images[currentImageIndex])
+        showImage(index: currentImageIndex)
+    }
+    
+    func showImage(index: Int) {
+        let imageName = images[index]
+        imageView.image = UIImage(named: imageName)
+        
+        leftButton.isEnabled = index > 0
+        rightButton.isEnabled = index < images.count - 1
+    }
+    
+    @IBAction func handleLike(_ sender: Any) {
+        likeButton.isSelected = !likeButton.isSelected
+    }
+    
+    let dateFormatter = DateFormatter()
+    @IBAction func handleDateChanged(_ sender: Any) {
+        dateFormatter.dateFormat = "yyyy년 M월 d일"
+        let dateStr = dateFormatter.string(from: datePicker.date)
+        dateLabel.text = "예약날짜 : \(dateStr)"
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        leftButton.isEnabled = false
+        dateFormatter.dateStyle = .medium
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -35,7 +82,8 @@ class ViewController: UIViewController {
         depthLabel.text = "68cm"
         widthLabel.text = "68cm"
         heightLabel.text = "110cm"
+        
+        handleDateChanged(datePicker)
     }
-
 }
 
